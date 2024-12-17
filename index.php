3c8fcbc10f2af7fe9375b4a4c5d023de8bc5d17a
thaiho101 <102989403+thaiho101@users.noreply.github.com>
2024-12-16 20:43:36 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-Hello World!
-Testing!!!
-adljgklajd
+<!DOCTYPE html>
+<html lang='en'>
+<br>
+<head>
+        <meta charset="UTF-8">
+        <title>Market Cost</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+        <link rel="icon" type="image/x-icon" href="/Icon/shoppingCart2.png">
+</head>
+<body>
+    <div id='navigation'>Market Cost
+    </div>
+
+    <div id='bottom'>
+    </div>
+</body>
+</html>
