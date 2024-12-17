e8c869215f4b6f21af4f245ad51336cc89c60906
thaiho101 <102989403+thaiho101@users.noreply.github.com>
2024-12-16 21:10:28 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,42 +1,30 @@
 <!DOCTYPE html>
-<html lang='en'>
+<html lang="en">
 <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Market Cost</title>
-        <link rel="stylesheet" href="style.css">
-        <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"> -->
-        <!-- <link rel="icon" type="image/x-icon" href="/Icon/shoppingCart2.png"> -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Market Cost</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="icon" type="image/x-icon" href="/Icon/shoppingCart2.png">
 </head>
 <body>
-    <div id='navigation'>Market Cost
-    </div>
+    <div id="navigation">Market Cost</div>
 
-    <div id='bottom'>
-        <div id='dataChange'>
-            <div class='insert'>Insert
-            </div>
-
-            <div class='budget'>Budget
-            </div>
+    <div id="bottom">
+        <div id="dataChange">
+            <div class="insert">Insert</div>
+            <div class="budget">Budget</div>
         </div>
 
-        <div id='dataAnalyze'>
-            <div class='filter'>Filter
-            </div>
-            
-            <div class='header'>Header
-            </div>
-
-            <div class='content'>Content
-            </div>
-
-            <div class='totalSummary'>Total
-            </div>
+        <div id="dataAnalyze">
+            <div class="filter">Filter</div>
+            <div class="header">Header</div>
+            <div class="content">Content</div>
+            <div class="totalSummary">Total</div>
         </div>
 
-        <div id='graph'>Graph
-        </div>
+        <div id="graph">Graph</div>
     </div>
 </body>
 </html>
