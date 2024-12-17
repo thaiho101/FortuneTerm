3562d4a093666f03412aff24d0b84ebdf6c78810
thaiho101 <102989403+thaiho101@users.noreply.github.com>
2024-12-16 21:14:16 -0600

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>Market Cost</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link rel="icon" type="image/x-icon" href="/Icon/shoppingCart2.png">
+    <link rel="icon" type="image/x-icon" href="shoppingCart2.png">
 </head>
 <body>
     <div id="navigation">Market Cost</div>
