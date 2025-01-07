d080d0b29657514d7459a4e379fa17563e4b96ad
thaiho101 <102989403+thaiho101@users.noreply.github.com>
2025-01-07 17:32:32 -0600

    Update index.php

--- myAccount/index.php
+++ myAccount/index.php
@@ -19,7 +19,7 @@ require_once('../config.php');
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Market Cost</title>
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="../style.css">
         <!-- <link rel="stylesheet" href="/myProfile/style1.css"> -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
         <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
