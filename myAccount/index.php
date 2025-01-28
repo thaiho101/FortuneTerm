5f9d81dc2fc532845ebb8b6e723984f48290f83a
thaiho101 <102989403+thaiho101@users.noreply.github.com>
2025-01-28 12:05:27 -0600

    fix bugs for theme styles paths

--- myAccount/index.php
+++ myAccount/index.php
@@ -133,7 +133,7 @@ if ($_SESSION['languageType'] == 'English') {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Market Cost</title>
         <?php //$theme was implemented in /Components/theme.php?>
-        <link rel="stylesheet" href="<?php echo "/theme/" . $theme?>">
+        <link rel="stylesheet" href="<?php echo "../theme/" . $theme?>">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
         <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
 
